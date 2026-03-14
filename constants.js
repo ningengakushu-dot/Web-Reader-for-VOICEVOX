b5189a5f522690989d4fedad7f29ed9fde0106c6
ningengakushu-dot <ningengakushu@gmail.com>
2026-03-14 11:50:53 +0900

    リファクタリングのみ

---
+++ constants.js
@@ -0,0 +1,14 @@
+// 共通定数定義
+
+// VOICEVOXエンジンのベースURL（ローカルサーバー）
+const VOICEVOX_BASE_URL = "http://127.0.0.1:50021";
+
+// 設定のデフォルト値
+const SETTING_DEFAULTS = {
+    speakerId: 1,
+    speedScale: 1.0,
+    pitchScale: 0.0,
+    intonationScale: 1.0,
+    volumeScale: 1.0,
+    pauseLengthScale: 1.0
+};
